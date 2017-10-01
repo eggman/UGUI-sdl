c1c9639654e377160fd6aa7e3243d27c39d3bef0
eggman <skouhei@gmail.com>
2017-10-01 22:08:24 +0900

    create sdl screen.

--- src/sdl.c
+++ src/sdl.c
@@ -1,5 +1,27 @@
+#include "SDL.h"
+
+static SDL_Surface *screen;
 
 int main (int argc, char **argv)
 {
+    SDL_Event e;
+    int done = 0;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    screen = SDL_SetVideoMode(480,320,24,SDL_HWSURFACE);
+
+    do{
+        if (SDL_PollEvent(&e) != 0){
+            switch(e.type){
+            case SDL_QUIT:
+                done = 1;
+                break;
+            }
+        }
+
+        SDL_UpdateRect(screen,0,0,0,0);
+    }while(!done);
+
+    SDL_Quit();
     return 0;
 }
